794c2c52214c9cef616a652b78d7b3298fcefb12
Liam <95448250+LRGuess@users.noreply.github.com>
2024-06-25 14:20:50 -0600

    Made more styles and LD button is coming

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import './app.css'
 import Classroom from './Classroom'; 
 import Student from './Student';
+
 class App extends React.Component {
   render() {
     return (
-      <div className="App" data-theme="light">
+      <div className="App light-mode" data-theme="light">
         <div className='container'>
-          <h1 className="title">Savvy picker</h1>
+          <div>
+            <h1 className="title header-margin">Savvy picker</h1>
+            <button class="button is-size-0.1">Student Picker</button>
+          </div>
           <hr className="is-divider" />
           <p className='is-uppercase has-text-white'>Current Classes:</p>
           <br></br><br></br>
         </div>
-        <Classroom />
+        <Classroom class_name={"9-2"} classStudentAmount={26}/>
+        <Classroom class_name={"da bois"} classStudentAmount={46}/>
+        <Classroom class_name={"DOMI SIKORAS CLASSY CLASS"} classStudentAmount={16}/>
+        <Classroom class_name={"SNIFF"} classStudentAmount={3}/>
       </div>
     );
   }
--- frontend/src/Classroom.js
+++ frontend/src/Classroom.js
@@ -3,17 +3,14 @@ import React from 'react';
 import 'bulma/css/bulma.min.css';
 import './Classroom.css'
 
-let class_name = "9-2"
-let classStudentAmount = "29"
 
-class Classroom extends React.Component {
-  render() {
+function Classroom({class_name, classStudentAmount}) {
     return (
-      <div className="container">
+      <div className="container ClassesListSpacing">
         <div class="card">
             <div class="card-content">
                 <div class="content">
-                    <span class='has-text-primary mr-6 ComponentAlignment'>{class_name}</span>
+                    <span class='has-text-primary is-size-5 mr-6 ComponentAlignment'>{class_name}</span>
                     <button class="button has-background-info-light has-text-info is-size-0.1 py-0 px-15 ComponentAlignment">Manage {classStudentAmount} Students</button>
                     <button class="button is-size-0.1 ClassesButtonSpacing has-background-warning-light has-text-warning">Student Picker</button>
                     <button class="button is-size-0.1 ChoserButtonSpacing has-background-danger-light has-text-danger">Team Creator</button>    
@@ -22,7 +19,6 @@ class Classroom extends React.Component {
         </div>
       </div>
     );
-  }
 }
 
 export default Classroom;
