33db4b50e9aa6f16b190efdeb6a665a19db1a86e
Liam <95448250+LRGuess@users.noreply.github.com>
2024-06-25 10:38:37 -0600

    Update Classroom.js

--- frontend/src/Classroom.js
+++ frontend/src/Classroom.js
@@ -1,12 +1,14 @@
+// src/Classroom.js
 import React from 'react';
-import 'bulma/css/bulma.min.css'
+import 'bulma/css/bulma.min.css';
 
 class Classroom extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to the Classroom</h1>
-        <p>This is the Classroom component.</p>
+      <div className="container">
+        <h1 className="title">Savvy picker</h1>
+        <hr className="is-divider" />
+        <p className='is-uppercase has-text-white'>This is the Classroom component.</p>
       </div>
     );
   }
