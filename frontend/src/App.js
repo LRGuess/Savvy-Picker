49cd3ce07774358a9eba486ef3b7f4658d43a5ea
Liam <95448250+LRGuess@users.noreply.github.com>
2024-06-25 12:19:18 -0600

    Created Basic Classroom.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Classroom from './Classroom'; 
-import Student from './Student';// Adjust the import path as necessary
-
+import Student from './Student';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" data-theme="light">
+        <div className='container'>
+          <h1 className="title">Savvy picker</h1>
+          <hr className="is-divider" />
+          <p className='is-uppercase has-text-white'>Current Classes:</p>
+          <br></br><br></br>
+        </div>
         <Classroom />
-        <Student/>
       </div>
     );
   }
--- frontend/src/Classroom.js
+++ frontend/src/Classroom.js
@@ -1,14 +1,25 @@
 // src/Classroom.js
 import React from 'react';
 import 'bulma/css/bulma.min.css';
+import './Classroom.css'
+
+let class_name = "9-2"
+let classStudentAmount = "29"
 
 class Classroom extends React.Component {
   render() {
     return (
       <div className="container">
-        <h1 className="title">Savvy picker</h1>
-        <hr className="is-divider" />
-        <p className='is-uppercase has-text-white'>This is the Classroom component.</p>
+        <div class="card">
+            <div class="card-content">
+                <div class="content">
+                    <span class='has-text-primary mr-6 ComponentAlignment'>{class_name}</span>
+                    <button class="button has-background-info-light has-text-info is-size-0.1 py-0 px-15 ComponentAlignment">Manage {classStudentAmount} Students</button>
+                    <button class="button is-size-0.1 ClassesButtonSpacing has-background-warning-light has-text-warning">Student Picker</button>
+                    <button class="button is-size-0.1 ChoserButtonSpacing has-background-danger-light has-text-danger">Team Creator</button>    
+                </div>
+            </div>
+        </div>
       </div>
     );
   }
